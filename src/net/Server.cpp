1c256b44b18cc01b1a0f69d3ea45ae6e5aa2c084
Mathias Roustan <roustan_mathias@outlook.fr>
2022-07-12 20:12:32 +0200

    fix buffer

--- src/net/Server.cpp
+++ src/net/Server.cpp
@@ -3,6 +3,8 @@
 #include <sys/socket.h>
 #include <poll.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <cstring>
 
 #include <iostream>
 #include <string>
@@ -61,45 +63,56 @@ void Server::poll()
 {
 	std::vector<struct pollfd> pool;
 
-	for (std::vector<Client *>::const_iterator it = this->_clients.begin(); it != this->_clients.end(); ++it) {
+	for (std::vector<Client *>::const_iterator it = this->_clients.begin(); it != this->_clients.end(); ++it)
+	{
 		struct pollfd pfd = {
 			.fd = (*it)->socket().raw(),
 			.events = POLLIN | POLLHUP,
-			.revents = 0
-		};
+			.revents = 0};
 		pool.push_back(pfd);
 	}
 
 	::poll(&pool[0], pool.size(), 15);
 
-	for (size_t i = 0; i < pool.size(); ++i) {
-		if (pool[i].revents & POLLHUP) {
+	for (size_t i = 0; i < pool.size(); ++i)
+	{
+		if (pool[i].revents & POLLHUP)
+		{
 			std::cout << this->_clients[i]->address().as_string() << " as disconnected" << std::endl;
 			this->_clients.erase(this->_clients.begin() + i);
 			pool.erase(pool.begin() + i);
 			--i;
-			continue ;
-		} else if (pool[i].revents & POLLIN) {
+			continue;
+		}
+		else if (pool[i].revents & POLLIN)
+		{
 			std::cout << this->_clients[i]->address().as_string() << " has data to send" << std::endl;
 			std::string packet;
-			char *buf = new char[1024];
-			buf[0] = 0;
-			for (;;) {
-				ssize_t stat = ::recv(pool[i].fd, buf, 1024, 0);
-				if (stat == 0) {
-					break ;
-				} else if (stat == -1) {
+			char buf[1024] = {0};
+			for (;;)
+			{
+				std::memset(buf, 0, sizeof(buf));
+				ssize_t stat = ::recv(pool[i].fd, buf, 1023, 0);
+				if (stat == 0)
+				{
+					break;
+				}
+				else if (stat == -1)
+				{
 					std::cerr << "Error reading data from " << this->_clients[i]->address().as_string() << std::endl;
 					perror("recv");
 					exit(-1);
-				} else {
-					for (ssize_t j = 0; j < stat; ++j) {
-						packet += buf[i];
+				}
+				else
+				{
+					for (ssize_t j = 0; j < stat; ++j)
+					{
+						packet += buf[j];
 					}
 				}
 			}
-			delete[] buf;
-			if (!packet.empty()) {
+			if (!packet.empty())
+			{
 				std::cout << "Message: " << packet << std::endl;
 			}
 		}
