e215dd194565db6e28b56f71d244309e121fb034
Mathias Roustan <roustan_mathias@outlook.fr>
2022-07-20 19:07:31 +0200

    FInished QUIT

--- src/commands/QUIT.cpp
+++ src/commands/QUIT.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "Server.hpp"
+#include "Channel.hpp"
 #include "User.hpp"
 #include "Commands.hpp"
 
@@ -25,6 +26,14 @@ void	Quit::execute(ft::User *sender, const std::string& msg)
 {
 	this->parse(msg);
 
+	/*
+		The RFC states: 'A client session is terminated with a quit message.
+		The server acknowledges this by sending an ERROR message to the client.'
+
+		This will never be sended back client because poll is disconnected before
+	*/
+	sender->send(ERROR("Disconnected"));
+
 	int i = this->_server.find_user_index(*sender);
 	if (i >= 0) {
 		this->_server.request_disconnect(i);
@@ -32,12 +41,10 @@ void	Quit::execute(ft::User *sender, const std::string& msg)
 		std::cerr << "WARNING: User quit but was not connected. This should NOT happen" << std::endl;
 	}
 
-	// TODO: Search every channel where the sender is and send a PRIVMSG with this->_reason
-
-	/* TODO:
-		The RFC states: 'A client session is terminated with a quit message.  The server
-		acknowledges this by sending an ERROR message to the client.'
-	*/
+	std::vector<Channel *> channels = this->_server.get_channels_with_user(sender);
+	for (std::vector<Channel *>::iterator channel = channels.begin(); channel != channels.end(); ++channel) {
+		(*channel)->dispatch_message(sender, PART(sender->nick(), sender->username(), (*channel)->name(), this->_reason));
+	}
 }
 
 }}
